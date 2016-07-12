47318c9459bcf41a03b5ed1536f6d830d72f8c39
ngrepos <mail2gnk@gmail.com>
2016-07-12 06:54:48 -0500

    Used jshint jscs wiredep

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ var express = require("express");
 
 var app = express();
 
-var port = 2000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function(req, res) {
-    res.send("Hello Node");
-});
+    res.send("Hello Node");}
+);
 
 app.get('/Books', function (req, res) {
     res.send("hello books");
-})
+});
 
 app.listen(port, function (err) {
     console.log("Running on port " + port);
