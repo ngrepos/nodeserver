44706517f553383d0b838e6ecc7d6768e5e7bc3e
ngrepos <mail2gnk@gmail.com>
2016-07-11 21:28:37 -0500

    Node server

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+var app = express();
+
+var port = 2000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res) {
+    res.send("Hello Node");
+});
+
+app.get('/Books', function (req, res) {
+    res.send("hello books");
+})
+
+app.listen(port, function (err) {
+    console.log("Running on port " + port);
+});
